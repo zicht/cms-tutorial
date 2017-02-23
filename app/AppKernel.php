bd630a3d946dcd5456abd0706ae5e8b2fd591d35
Gerard van Helden <gerard@zicht.nl>
2017-02-23 13:32:28 +0100

    add AppKernel.php

---
+++ app/AppKernel.php
@@ -0,0 +1,17 @@
+<?php
+
+use Zicht\SymfonyUtil\HttpKernel\Kernel;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+        ];
+    }
+}
