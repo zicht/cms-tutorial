c72953bea27024802f45db0b9f6d28c6133b8897
Gerard van Helden <gerard@zicht.nl>
2017-02-28 10:30:45 +0100

    add DI extension

---
+++ src/SiteBundle/DependencyInjection/AcmeSiteExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Acme\SiteBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection as DI;
+
+class AcmeSiteExtension extends DI\Extension\Extension
+{
+    public function load(array $configs, DI\ContainerBuilder $container)
+    {
+        $loader = new DI\Loader\XmlFileLoader(
+            $container, 
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('admin.xml');
+    }
+}
+
